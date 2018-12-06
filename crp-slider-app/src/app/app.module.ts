b0e26cf8c6cff036ffd289fadd50b934051ad040
Tara Matthews <tara.matthews@etventure.com>
2018-12-06 15:29:58 +0100

    routes

--- crp-slider-app/src/app/app.module.ts
+++ crp-slider-app/src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+import { ButtonComponent } from './button/button.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'slider',
+    pathMatch: 'full'
+  },
+  {
+    path: 'slider',
+    component: SliderComponent
+  },
+  {
+    path: 'button',
+    component: ButtonComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    ButtonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
