e2af290cd1c0a9a9c241766ec0e8a0f4913e92de
Tara Matthews <tara.matthews@etventure.com>
2018-12-06 22:49:05 +0100

    firebase prod

--- crp-slider-app/src/environments/environment.prod.ts
+++ crp-slider-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDlXh5wy4zt06TpBab_6EatamQMVs-qcK4",
+    authDomain: "crp-slider-app.firebaseapp.com",
+    databaseURL: "https://crp-slider-app.firebaseio.com",
+    projectId: "crp-slider-app",
+    storageBucket: "crp-slider-app.appspot.com",
+    messagingSenderId: "271026784079"
+  }
 };
